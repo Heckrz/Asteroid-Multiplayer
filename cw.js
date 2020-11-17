541c1bf433bf0983403492a9945599e4157822a0
mk2008 <mk2008@hw.ac.uk>
2020-11-17 18:00:21 +0400

    @sms200 and @zfr18 worked on this

--- cw.js
+++ cw.js
@@ -1,1 +1,23 @@
-alert("hello world this is game")
+var create = function(type, size)
+ {
+ var obj = Object.create(def);
+ obj.init(type, size);
+ 
+ return obj;
+ };
+ 
+ var def =
+ {
+ _type: null,
+ _size: null,
+ 
+ init: function(type, size)
+ {
+ this._type = type;
+ this._size = size;
+ 
+ var i = 0;
+ var length = this._size;
+ 
+ 
+ }, 
