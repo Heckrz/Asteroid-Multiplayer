5e5e1a7e17f98c8943dee3ddb6074011f2c79356
jsg2000 <jsg2000@hw.ac.uk>
2020-11-19 09:21:39 +0400

    added fiunal html some css and declared variables

--- cw.js
+++ cw.js
@@ -1,23 +1,31 @@
-var create = function(type, size)
- {
- var obj = Object.create(def);
- obj.init(type, size);
- 
- return obj;
- };
- 
- var def =
- {
- _type: null,
- _size: null,
- 
- init: function(type, size)
- {
- this._type = type;
- this._size = size;
- 
- var i = 0;
- var length = this._size;
- 
- 
- }, 
+var username = prompt("What is your name?");
+var gameCanvas = document.getElementById('game');
+var player = document.getElementById('sprite');
+var bullet_container = document.getElementById('bullet-container');
+var ennemy_container = document.getElementById('ennemy-container');
+var heart_container = document.getElementById('hearts');
+var hearts = Array.from(document.querySelectorAll('.heart'));
+var scoreText = document.getElementById('score');
+var levelText = document.getElementById('level');
+var highScoreText = document.getElementById('highScore');
+var introText = document.getElementById('intro');
+
+var highScore = 0;
+var score = 0;
+var level = 0;
+var healthpoints = hearts.length;
+
+var pause = true;
+
+var ennemies = [];
+var bullets = [];
+
+var lastTimeRender = 0;
+var lastTimeShot = 0;
+var lastTimeEnnemy  = 0;
+
+let ArrowDown = false;
+let ArrowUp = false;
+let ArrowLeft = false;
+let ArrowRight = false;
+let SpaceBar = false;
