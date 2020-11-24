271915503ae7415030f3703a383c95cfeb9f26cf
jsg2000 <jsg2000@hw.ac.uk>
2020-11-24 18:35:29 +0400

    added server side code only connecting left

---
+++ server2.js
@@ -0,0 +1,29 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Jagmeet@2002",
+  database: "testnew"
+});
+
+
+
+
+var name = "hello";
+var num = 6;
+
+
+
+
+con.connect(function(err) {
+    if (err) throw err;
+    con.query("insert into testn values ("+num+",'"+name+"');", function (err, result, fields) {
+        if (err) throw err;
+      console.log(result);
+    });
+    con.query("SELECT * FROM testn", function (err, result, fields) {
+        if (err) throw err;
+      console.log(result);
+    });
+});
